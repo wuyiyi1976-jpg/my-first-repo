edf076c7827048d9ffd2db9e6f1b988efccd34bc
wuyiyi1976-jpg <wuyiyi1976@gmail.com>
2026-04-17 12:09:29 +0800

    Fix greeting message and add greet function

--- greeting.py
+++ greeting.py
@@ -1,2 +1,7 @@
-Hell, my 1st branch greeting message !
-How R U?
+# Hello, my first branch greeting message!
+def greet(name):
+    return f"你好, {name}! 欢迎使用 GitHub。"
+
+if __name__ == "__main__":
+    user_name = input("请输入你的名字: ")
+    print(greet(user_name))
